987159e14def7aa70a48c29939c9c4887ebed89c
OriGuyUniqueDev <origuydev@gmail.com>
2023-06-26 20:33:18 +0300

    add about page

---
+++ src/components/LandingPage/About/About.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import { FunctionComponent } from "react";
+
+interface AboutProps {}
+
+const About: FunctionComponent<AboutProps> = () => {
+	return <div>About</div>;
+};
+
+export default About;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <LandingPage />,
+		children: [
+			{
+				path: "about",
+			},
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<React.StrictMode></React.StrictMode>);
