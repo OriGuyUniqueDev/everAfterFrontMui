04fe47208bb5b967857779b8dd4d0779aa6fc106
OriGuyUniqueDev <origuydev@gmail.com>
2023-07-07 11:10:50 +0300

    ui for the login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,70 +1,88 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Paper, Typography, Backdrop, Box, Fade, Modal, SxProps, Stack, Drawer } from "@mui/material";
+import { useFormik } from "formik";
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
 
 interface LoginPageProps {
 	handleClickOpen: () => void;
-	handleClose: () => void;
 	open: boolean;
 }
 
-const LoginPage: FunctionComponent<LoginPageProps> = ({ handleClose, handleClickOpen, open }) => {
-	const style: SxProps = {
-		position: "absolute",
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%, -50%)",
-		width: "auto",
-		bgcolor: "background.paper",
-		// border: "2px solid #000",
-		boxShadow: 24,
-		borderRadius: 5,
-		p: 4,
-	};
+const LoginPage: FunctionComponent<LoginPageProps> = ({ handleClickOpen, open }) => {
+	const formik = useFormik({
+		initialValues: {
+			email: "",
+			password: "",
+		},
+		validationSchema: yup.object({
+			email: yup.string().required().email("Invalid Email"),
+			password: yup.string().required().min(6, "Too Short, Should be at least 6 characters"),
+		}),
+		onSubmit(values) {
+			console.log(values);
+		},
+	});
 	return (
 		<>
 			<Drawer anchor={"bottom"} open={open} onClose={handleClickOpen}>
-				<Stack spacing={2} sx={{ mx: 10, my: 5 }}>
-					<Typography variant="h4">Lets Login 👋</Typography>
-					<Typography variant="body1" fontSize={20} color={"#fff"}>
-						Welcome, this is the first step to organize the wedding of your DREAMS{" "}
-					</Typography>
-					<TextField
-						InputLabelProps={{
-							placeholder: "Email Address",
-							style: { color: "#bbb" },
-						}}
-						sx={{ width: "20rem" }}
-						required
-						autoFocus
-						margin="dense"
-						id="email"
-						label="Email Address"
-						type="email"
-						variant="outlined"
-					/>
-					<TextField
-						InputLabelProps={{
-							placeholder: "Email Address",
-							style: { color: "#bbb" },
-						}}
-						sx={{ width: "20rem" }}
-						required
-						margin="dense"
-						id="password"
-						label="Password"
-						type="password"
-						variant="outlined"
-					/>
-					<Stack direction={"row"} spacing={3}>
-						<Button variant="contained" size="large" color="secondary" onClick={handleClickOpen}>
-							Login
-						</Button>
-						<Button variant="outlined" size="large" color="primary" onClick={handleClickOpen}>
-							Cancel
-						</Button>
+				<form onSubmit={formik.handleSubmit}>
+					<Stack spacing={2} sx={{ mx: 10, my: 5 }}>
+						<Typography variant="h4">Lets Login 👋</Typography>
+						<Typography variant="body1" fontSize={20} color={"#fff"}>
+							Welcome, this is the first step to organize the wedding of your DREAMS{" "}
+						</Typography>
+						<TextField
+							InputLabelProps={{
+								placeholder: "Email Address",
+								style: { color: "#bbb" },
+							}}
+							sx={{ width: "20rem" }}
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							required
+							autoFocus
+							margin="dense"
+							id="email"
+							label="Email Address"
+							type="email"
+							variant="outlined"
+							name="email"
+							error={formik.touched.email && Boolean(formik.errors.email)}
+							helperText={formik.touched.email && formik.errors.email}
+						/>
+						<TextField
+							InputLabelProps={{
+								placeholder: "Email Address",
+								style: { color: "#bbb" },
+							}}
+							sx={{ width: "20rem" }}
+							value={formik.values.password}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							name="password"
+							required
+							margin="dense"
+							id="password"
+							label="Password"
+							type="password"
+							variant="outlined"
+							error={formik.touched.password && Boolean(formik.errors.password)}
+							helperText={formik.touched.password && formik.errors.password}
+						/>
+
+						<Stack direction={"row"} spacing={3}>
+							<Button type="submit" variant="contained" size="large" color="secondary">
+								Login
+							</Button>
+							<Button variant="outlined" size="large" color="primary" onClick={handleClickOpen}>
+								Cancel
+							</Button>
+						</Stack>
 					</Stack>
-				</Stack>
+				</form>
 			</Drawer>
 		</>
 	);
