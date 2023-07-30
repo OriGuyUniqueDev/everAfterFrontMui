50659f06ab806661969a2836cc1aec6b51b75820
OriGuyUniqueDev <origuydev@gmail.com>
2023-07-30 20:51:05 +0300

    fix error ondeploy

--- src/interfaces/EventToServerType.ts
+++ src/interfaces/EventToServerType.ts
@@ -1,14 +1,18 @@
 export default interface EventToServerType {
-	numOfGuest: number;
-	eventUser: string;
-	eventPlanner: string;
-	hasEventPlanner: boolean;
-	hasVenue: boolean;
-	venueName: string;
-	dateOfWedding: string;
-	budget: number;
-	presents: number;
-	mealPrice: number;
-	tasks: string[];
-	_id: string;
+	numOfGuest?: number;
+	eventUser?: string;
+	eventPlanner?: string;
+	hasEventPlanner?: boolean;
+	hasVenue?: boolean;
+	venueName?: string;
+	dateOfWedding?: string;
+	leftToSpend?: number;
+	alreadyPaid?: number;
+	totalBudget?: number;
+	totalSpent?: number;
+	budget?: number;
+	presents?: number;
+	mealPrice?: number;
+	tasks?: string[];
+	_id?: string;
 }
